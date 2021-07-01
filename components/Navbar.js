e8aa504d920ff9af5557cd639fc2e22110a37abe
Ariel Talbot <Ariel.Talbot@iconfitness.com>
2021-06-30 23:12:29 -0600

    adding mobile hamburger menu with useState

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,19 +1,50 @@
 import Link from 'next/link'
+import { useState } from 'react'
 import styles from '../styles/Nav.module.css'
 
 const Navbar = () => {
+  const [active, setActive] = useState(false);
+
+  const handleClick = () => {
+    setActive(!active);
+  };
   return (
-    <nav className={styles.container}>
-      <div className={styles.wrapper}>
-        <div className="logo">
-          Portfolio
+    <nav>
+      <div className={styles.container}>
+        
+        {/* desktop menu */}
+        <div className={ `${styles.wrapper} ${styles.hideMobile}`}>
+          <div>
+            Portfolio
+          </div>
+          <div className={styles.linkContainer}>
+            <Link href="/"><a>Home</a></Link>
+            <Link href="/about"><a>About</a></Link>
+            <Link href="/"><a>Contact</a></Link>
+          </div>
         </div>
-        <div className={styles.linkContainer}>
-          <Link href="/"><a>Home</a></Link>
-          <Link href="/about"><a>About</a></Link>
-          <Link href="/"><a>Contact</a></Link>
+        
+        {/* mobile hamburger menu */}
+        <div className={styles.showMobile}>
+          <div className={styles.wrapper}>
+            <div className={styles.tabName}>
+              Portfolio
+            </div>
+            <button className={styles.hamburgerButton} onClick={handleClick}>
+              <svg className={styles.hamLine} fill='none' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'/>
+              <svg className={styles.hamLine} fill='none' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'/>
+              <svg className={styles.hamLine} fill='none' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'/>
+            </button>
+          </div>
+          <div className={`${styles.linkContainer} ${active ? '' : 'hidden'}`}>
+            <Link href="/"><a>Home</a></Link>
+            <Link href="/about"><a>About</a></Link>
+            <Link href="/"><a>Contact</a></Link>
+          </div>
         </div>
       </div>
+
+      <div className={styles.border}></div>
     </nav>
     );
 }
